b9db34ecfb7abd32d78b951ad0c880ae0751d528
wang.xiaohu9 <wang.xiaohu9@iwhalecloud.com>
2023-07-11 20:18:11 +0800

    ### 0.4.0
    -   增加高风险文件检测 提醒功能

--- src/extension/globalState.ts
+++ src/extension/globalState.ts
@@ -1,14 +1,23 @@
 import { Memento } from "vscode";
 import { GenerateSession } from "./generate";
 
+interface rule {
+    file_type: string;
+    file_path: string;
+    comments: string;
+}
 interface GlobalState {
     activeSession: GenerateSession | null;
     storage: Memento | null;
+    remoteUrl: string | null;
+    ruleList: Array<rule>;
 }
 
 const globalState: GlobalState = {
     activeSession: null,
     storage: null,
+    remoteUrl: null,
+    ruleList: [],
 };
 
 export function getGlobalState() {
--- src/extension/index.ts
+++ src/extension/index.ts
@@ -1,6 +1,9 @@
 import * as vscode from "vscode";
 import * as crypto from "crypto";
-import * as fs from "node:fs";
+import axios from "axios";
+import { simpleGit} from 'simple-git';
+import { setTabnineExtensionContext } from "./huggingface/globals/tabnineExtensionContext";
+
 
 import { GenerateSession, getScratchpadManager } from "./generate";
 import { getGlobalState } from "./globalState";
@@ -50,6 +53,117 @@ async function handleGenerateCodeCommand() {
     setHasActiveGenerateSessionContext(true);
 }
 
+
+
+interface GitUrlParts {
+    gitHost: string;
+    orgName: string;
+    repoName: string;
+  }
+  
+  function splitGitUrl(url: string): GitUrlParts | null {
+    let giturl = url;
+    // 1. 规整像 ssh://git@git-nj.iwhalecloud.com:52422/ossr9dev/rc-public-capability-product.git 这样的git地址
+    if (url.startsWith('ssh://')) {
+        giturl = url.replace('ssh://','');        
+    } 
+
+    const regex = /^(?:https?|git)(?::\/\/|@)([^\/]+)[\/:]([^\/]+)\/(.+?)(?:\.git)?$/;
+    const matches = giturl.match(regex);
+  
+    if (matches) {
+      let gitHost = matches[1];
+      const orgName = matches[2];
+      const repoName = matches[3];
+      
+      // 2. 规整 git-nj.iwhalecloud.com:52422 这种情况
+      if (gitHost.indexOf(':') !== -1) {
+        gitHost = gitHost.replace(/:\d+$/, "");
+      }
+      return {
+        gitHost,
+        orgName,
+        repoName
+      };
+    } else {
+      return null; // 无效的 git 地址
+    }
+  }
+
+function initStartup(context: vscode.ExtensionContext): void {
+    setTabnineExtensionContext(context);
+
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      vscode.window.showErrorMessage('No workspace folder found.');
+      return;
+    }
+
+    const workspaceFolder = workspaceFolders[0];
+    const workspaceFolderPath = workspaceFolder.uri.fsPath;
+
+    const git = simpleGit(workspaceFolderPath);    
+    git.getRemotes(true, (err, remotes) => {
+        if (err) {
+          //vscode.window.showErrorMessage('Failed to get git remote url.');
+          console.error(err);
+          return;
+        }
+  
+        const remoteUrl = remotes[0].refs.fetch;
+        getGlobalState().remoteUrl = remoteUrl;
+
+        // vscode.window.showWarningMessage('Git remote url: ' + remoteUrl);
+
+        const result = splitGitUrl(remoteUrl);
+
+        if (result) {
+            let data = JSON.stringify({
+                "git_host": result.gitHost,
+                "org_name": result.orgName,
+                "repo_name": result.repoName
+            });
+
+            console.log("git.getRemotes1.0---", data)
+        
+            let config = {
+                method: 'post',
+                url: 'https://dev.iwhalecloud.com/faas/serverless/codingplus/high-risk',
+                headers: { 
+                    'content-type': 'application/json'
+                },
+                data : data
+            };
+
+            console.log("git.getRemotes2.0---", config)
+    
+            axios.request(config).then((response) => {
+                console.log("git.getRemotes3.0---",  JSON.stringify(response.data));
+              const data = response.data;
+              if (data.rule_list) {
+                    getGlobalState().ruleList =  data.rule_list;
+              } 
+            })
+            .catch((error) => {
+                console.log("git.getRemotes4.0---",error);
+            });
+        }
+
+      });    
+
+    //   codingplus 获取高风险文件的接口 ： 
+    //   curl --request POST \
+    //     --url https://dev.iwhalecloud.com/faas/serverless/codingplus/high-risk \
+    //     --header 'content-type: application/json' \
+    //     --data '{
+    //     "git_host": "git-nj.iwhalecloud.com",
+    //     "org_name": "bss3",
+    //     "repo_name": "bss_pos_core"
+    //   }'
+
+
+  }
+
 export function activate(context: vscode.ExtensionContext) {
     // To use crypto features in WebAssembly, we need to add this polyfill.
     global.crypto = {
@@ -62,6 +176,20 @@ export function activate(context: vscode.ExtensionContext) {
     getGlobalState().storage = context.globalState;
 
     context.subscriptions.push(
+        vscode.workspace.onDidOpenTextDocument((document) => {
+            //  vscode.window.showInformationMessage('Opened document: ' + document.fileName);
+            // 判断是否是高风险  
+            getGlobalState().ruleList.forEach(element => {
+                if (document.fileName.indexOf(element.file_path) !== -1) {
+                    let strMsg = '该代码文件为风险代码，修改需谨慎！';
+                    if (element.comments && element.comments.length > 0) {
+                        strMsg = '该代码文件为风险代码，修改需谨慎！原因：' + element.comments 
+                    }
+                    vscode.window.showWarningMessage(strMsg);
+                }
+            });
+            
+        }),
         vscode.commands.registerCommand("whalecloud.generateCode", () => {
             handleGenerateCodeCommand();
         }),
@@ -98,8 +226,17 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.window.registerWebviewViewProvider(
             ChatPanelProvider.viewType,
             new ChatPanelProvider(context)
-        )
+        ),
+        
     );
+
+    // ---------------------begin huggingface
+    void initStartup(context);
+    
+
+    // ----------------------end huggingface
+
+
 }
 
 export function deactivate() {
@@ -107,4 +244,25 @@ export function deactivate() {
     globalState.activeSession?.dispose();
     globalState.activeSession = null;
     globalState.storage = null;
+}
+
+async function getRemoteUrl() {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (!workspaceFolders) {
+      vscode.window.showErrorMessage('No workspace folder found.');
+      return;
+    }
+
+    const workspaceFolder = workspaceFolders[0];
+    const workspaceFolderPath = workspaceFolder.uri.fsPath;
+
+    const git = simpleGit(workspaceFolderPath);
+    try {
+      const remotes = await git.getRemotes(true);
+      const remoteUrl = remotes[0].refs.fetch;
+      vscode.window.showWarningMessage('Git remote url: ' + remoteUrl);
+    } catch (err) {
+      vscode.window.showErrorMessage('Failed to get git remote url.');
+      console.error(err);
+    }
 }
